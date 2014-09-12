6f98563010f99e7e327fef79631a494ae913c488
Vipul <vipul@vipul.(none)>
2014-09-12 15:50:31 +0530

    change

---
+++ Railways_ticket/db/migrate/20140912101805_addreservation_idtocustomer.rb
@@ -0,0 +1,4 @@
+class AddreservationIdtocustomer < ActiveRecord::Migration
+  def change
+  end
+end
--- Railways_ticket/db/schema.rb
+++ Railways_ticket/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140911120950) do
+ActiveRecord::Schema.define(version: 20140912101858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,20 @@ ActiveRecord::Schema.define(version: 20140911120950) do
   end
 
   create_table "customers", force: true do |t|
+    t.string   "PNR"
+    t.string   "passenger_name"
+    t.string   "Gender"
+    t.string   "Age"
+    t.string   "Seat_no"
+    t.string   "Class"
+    t.string   "Fare"
+    t.string   "Source_id"
+    t.string   "Destination_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "passengers", force: true do |t|
     t.string   "PNR"
     t.string   "passenger_name"
     t.string   "Gender"
