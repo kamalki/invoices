c0d7d26aa897afa641f9304fd80dabfa07e9a2a0
Vipul <vipul@vipul.(none)>
2014-09-15 11:22:34 +0530

    msg

--- Railways_ticket/db/migrate/20140911100651_create_stations.rb
+++ Railways_ticket/db/migrate/20140911100651_create_stations.rb
@@ -1,7 +1,7 @@
 class CreateStations < ActiveRecord::Migration
   def change
     create_table :stations do |t|
-      t.integer :stationid_id
+      t.integer :station_id
       t.string :station_name
 
       t.timestamps
