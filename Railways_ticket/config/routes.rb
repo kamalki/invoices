adc1b511544a9ef3e2a6db4c976f939190bf4adb
Vipul <vipul@vipul.(none)>
2014-09-16 11:36:07 +0530

    changes

--- Railways_ticket/config/routes.rb
+++ Railways_ticket/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   
 
+  get 'welcome/index'
+
   resources :reservations
 
   resources :consists_ofs
@@ -22,7 +24,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
